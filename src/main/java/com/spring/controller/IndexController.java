f6c6c598470009b6617912bdbad77edeb584b9eb
Sasirekha Kumaran <sasirekha.kumaran@infor.com>
2024-11-19 12:39:41 +0530

    updated

--- src/main/java/com/spring/controller/IndexController.java
+++ src/main/java/com/spring/controller/IndexController.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controllers
 public class IndexController {
 
 	@GetMapping("/")
